7853a731df0f569b5061d1730a0174f8f885db63
North-WeDev <71936328+North-WeDev@users.noreply.github.com>
2020-10-07 10:26:05 -0400

    Add files via upload
    - Changed how the slide bar works
    - Side bar now uses JS instead of label check/uncheck hack
    - Made some UI changes for better consistency
    - Made some changes to the code to make the page more fluid(%, vm and vh units)
    - Made some tweaks to the code

---
+++ menu.js
@@ -0,0 +1,7 @@
+/*Menu*/
+function menu(){
+	var start=document.getElementById("side-bar");
+	if(start.style.transform=="translate3d(0px, 0px, 0px)"){start.style.transform="translate3d(-200px, 0px, 0px)"}
+	else{start.style.transform="translate3d(0px, 0px, 0px)"}
+	
+}
